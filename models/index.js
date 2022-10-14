bfa52440086424fe124a3a6046dc79dbd3ba6eb3
levu2207 <levu2207@gmail.com>
2022-10-14 21:12:24 +0700

    a

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 
 const db = {};
-
+console.log(config.database);
 let sequelize;
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
